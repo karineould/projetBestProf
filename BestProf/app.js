0711969ef7221535d0875f3f93cc6ab5189f862e
Karine Ouldbraham <k.ouldbraham@weezevent.com>
2016-03-10 02:24:03 +0100

    add template home page

--- BestProf/app.js
+++ BestProf/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cons = require('consolidate');
 
 var routes = require('./routes/index');
 var user = require('./routes/users');
@@ -14,12 +15,13 @@ var app = express();
 
 
 // view engine setup
+app.engine('html',cons.twig);
 app.set('models', path.join(__dirname, 'models'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
